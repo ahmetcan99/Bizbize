4d8ea6370c69515554f3b42e1ff98c9da6edeb6a
ahmetcan99 <acerleblebici@gmail.com>
2024-05-30 18:03:25 +0300

    docker added

--- Bizbize/settings.py
+++ Bizbize/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'Bizbize.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "bizbize",
+        'USER': 'postgrescan',
+        'PASSWORD': '1234',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
